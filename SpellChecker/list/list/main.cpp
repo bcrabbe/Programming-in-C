c80dccdbed4a73150a48356085713992f93201ef
bcrabbe <bencrabbe@hotmail.co.uk>
2014-11-21 11:35:16 +0000

    Initial Commit

---
+++ SpellChecker/list/list/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  list
+//
+//  Created by ben on 21/11/2014.
+//  Copyright (c) 2014 ben. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
